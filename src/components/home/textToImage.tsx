a1d17028794f2b9b36279e57233d863ab6e23a7b
Avazbek-Khudoynazarov <justdanny9812@gmail.com>
2024-11-26 15:12:26 +0900

    feat: text-to-image

--- src/components/home/textToImage.tsx
+++ src/components/home/textToImage.tsx
@@ -9,16 +9,35 @@ import ArrowDropDownRoundedIcon from "@mui/icons-material/ArrowDropDownRounded";
 import TextField from "@mui/material/TextField";
 import { Box, Slider, Typography } from "@mui/material";
 import Link from "next/link";
+import Fade from "@mui/material/Fade";
+
 import PopupState, { bindTrigger, bindMenu } from "material-ui-popup-state";
 
 import "./css/textToImage.css";
 import Image from "next/image";
+import axios from "axios";
 
 export default function TextToImage() {
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
-  const [number, setNumber] = useState(0);
+  const [prompt, setPrompt] = useState("");
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+  const [negativePrompt, setNegativePrompt] = useState("");
+  const [width, setWidth] = useState(512);
+  const [height, setHeight] = useState(512);
+  const [count, setCount] = useState(1);
+  const [selectedModel, setSelectedModel] = useState("Stable Diffusion XD 2.0");
+
+  const [steps, setSteps] = useState(70);
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
   const pathname = usePathname();
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
   console.log("Current Pathname:", pathname);
 
@@ -29,10 +48,62 @@ export default function TextToImage() {
   const handleHeightChange = (event: Event, newValue: number | number[]) => {
     setHeight(newValue as number);
   };
-  const handleNumberChange = (event: Event, newValue: number | number[]) => {
-    setNumber(newValue as number);
+  const handleCountChange = (event: Event, newValue: number | number[]) => {
+    setCount(newValue as number);
   };
-  const images = Array(4).fill("./assets/home/girl.svg");
+
+  const handleStepsChange = (event: Event, newValue: number | number[]) => {
+    setSteps(newValue as number);
+  };
+
+  const handleGenerateImage = async () => {
+    if (!prompt) {
+      alert("Please enter a prompt!");
+      return;
+    }
+
+    const payload = {
+      prompt: `${prompt}, hyper-realistic, ultra-detailed, 8k resolution, photographic quality, highly realistic lighting, accurate textures, intricate details`,
+      negative_prompt: `${negativePrompt}, cartoon, anime, blurry, 3d render, painting, unrealistic`,
+      styles: ["photorealistic"],
+      sampler_name:
+        selectedModel === "Stable Diffusion XL"
+          ? "DPM++ SDE Karras"
+          : "DPM++ 2M Karras",
+      width: width,
+      height: height,
+      steps: 100,
+      cfg_scale: 8,
+      n_iter: count,
+      batch_size: 1,
+    };
+
+    try {
+      setIsLoading(true);
+      const response = await axios.post(
+        "http://ai.yeongnam.com:7860/sdapi/v1/txt2img",
+        payload
+      );
+      setImages(response.data.images);
+    } catch (error) {
+      console.error("Error generating image:", error);
+      alert("Failed to generate images. Please try again.");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  const handleDownload = (base64Image: string, index: number): void => {
+    const link = document.createElement("a");
+
+    link.download = `generated-image-${index + 1}.png`;
+
+    link.href = `data:image/png;base64,${base64Image}`;
+
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
 
   return (
     <div className="image-editor-main">
@@ -51,7 +122,6 @@ export default function TextToImage() {
             </span>
           </Link>
         </div>
-
         <div
           className={`from-text-page ${
             pathname === "/from-image" ? "active" : ""
@@ -70,7 +140,6 @@ export default function TextToImage() {
             </span>
           </Link>
         </div>
-
         <div
           className={`from-text-page ${
             pathname === "/in-painting" ? "active" : ""
@@ -97,37 +166,63 @@ export default function TextToImage() {
         </div>
         <div className="model-menu-position">
           <span className="title-span-style">Model</span>
-          <PopupState variant="popover" popupId="demo-popup-menu">
-            {(popupState) => (
-              <React.Fragment>
-                <Button
-                  className=" menu-btn-style"
-                  variant="contained"
-                  {...bindTrigger(popupState)}
-                >
-                  Stable Diffusion XD 2.0
-                  <ArrowDropDownRoundedIcon
-                    fontSize="large"
-                    sx={{ color: "#00504B" }}
-                  />
-                </Button>
-                <Menu {...bindMenu(popupState)}>
-                  <MenuItem onClick={popupState.close}>
-                    Stable Diffusion XD 2.0
-                  </MenuItem>
-                  <MenuItem onClick={popupState.close}>
-                    Stable Diffusion XD 2.1
-                  </MenuItem>
-                  <MenuItem onClick={popupState.close}>
-                    Stable Diffusion XL
-                  </MenuItem>
-                  <MenuItem onClick={popupState.close}>
-                    Stable Diffusion Lite
-                  </MenuItem>
-                </Menu>
-              </React.Fragment>
-            )}
-          </PopupState>
+          <Button
+            className="menu-btn-style"
+            id="fade-button"
+            aria-controls={open ? "fade-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleClick}
+          >
+            {selectedModel}{" "}
+            <ArrowDropDownRoundedIcon
+              fontSize="large"
+              sx={{ color: "#00504B" }}
+            />
+          </Button>
+          <Menu
+            id="fade-menu"
+            MenuListProps={{
+              "aria-labelledby": "fade-button",
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            TransitionComponent={Fade}
+          >
+            <MenuItem
+              onClick={() => {
+                setSelectedModel("Stable Diffusion XD 2.0");
+                handleClose();
+              }}
+            >
+              Stable Diffusion XD 2.0
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                setSelectedModel("Stable Diffusion XL");
+                handleClose();
+              }}
+            >
+              Stable Diffusion XL
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                setSelectedModel("Stable Diffusion Lite");
+                handleClose();
+              }}
+            >
+              Stable Diffusion Lite
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                setSelectedModel("Custom Model");
+                handleClose();
+              }}
+            >
+              Custom Model
+            </MenuItem>
+          </Menu>
         </div>
         <div className="prompt-input">
           <span className="title-span-style">Prompt</span>
@@ -138,6 +233,8 @@ export default function TextToImage() {
             fullWidth
             multiline
             minRows={3}
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
             sx={{
               backgroundColor: "#d8f1f1",
               borderRadius: "8px",
@@ -164,11 +261,13 @@ export default function TextToImage() {
           <span className="title-span-style">Negative Prompt</span>
           <TextField
             className="input-style-fix"
-            placeholder="e.g. A cat is sitting on a table"
+            placeholder="e.g. blurry, distorted"
             variant="outlined"
             fullWidth
             multiline
             minRows={3}
+            value={negativePrompt}
+            onChange={(e) => setNegativePrompt(e.target.value)}
             sx={{
               backgroundColor: "#d8f1f1",
               borderRadius: "8px",
@@ -194,102 +293,124 @@ export default function TextToImage() {
         <div className="slider-image-size">
           <span className="title-span-style">Output Size</span>
           <Box className="main-slider-image">
-            <div
-              style={{
+            <Box
+              sx={{
                 display: "flex",
+                flexDirection: "row",
+                gap: "15px",
                 alignItems: "center",
-                gap: 15,
-                marginBottom: "15px",
               }}
             >
-              <Typography variant="body1" sx={{ paddingRight: "5px" }}>
+              <Typography
+                variant="body1"
+                sx={{ display: "flex", width: "50px" }}
+              >
                 Width
               </Typography>
-              <div>
-                <Slider
-                  value={width}
-                  onChange={handleWidthChange}
-                  min={0}
-                  max={2000}
-                  className="slider-size-style"
-                  sx={{
-                    color: "#2f7367",
-                    "& .MuiSlider-track": {
-                      height: 16,
-                      borderRadius: 4,
-                    },
-                    "& .MuiSlider-rail": {
-                      height: 16,
-                      borderRadius: 4,
-                      backgroundColor: "#bbd5d1",
-                    },
-                    "& .MuiSlider-thumb": {
-                      zIndex: 99,
-                    },
-                  }}
-                />
-              </div>
+              <Slider
+                value={width}
+                onChange={handleWidthChange}
+                min={256}
+                max={2048}
+                step={256}
+                className="slider-size-style"
+                sx={{
+                  color: "#2f7367",
+                  "& .MuiSlider-track": {
+                    height: 16,
+                    borderRadius: 4,
+                  },
+                  "& .MuiSlider-rail": {
+                    height: 16,
+                    borderRadius: 4,
+                    backgroundColor: "#bbd5d1",
+                  },
+                  "& .MuiSlider-thumb": {
+                    zIndex: 99,
+                  },
+                }}
+              />
               <Box className="slider-size-displayer">
                 <span>{width}</span>
                 <span>px</span>
               </Box>
-            </div>
-            <Box className="main-slider-image">
-              <div
-                style={{
-                  display: "flex",
-                  alignItems: "center",
-                  gap: 15,
-                  marginBottom: "15px",
-                }}
+            </Box>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                gap: "15px",
+                alignItems: "center",
+              }}
+            >
+              <Typography
+                variant="body1"
+                sx={{ display: "flex", width: "50px" }}
               >
-                <Typography variant="body1">Height</Typography>
-                <div>
-                  <Slider
-                    value={height}
-                    onChange={handleHeightChange}
-                    min={0}
-                    max={2000}
-                    className="slider-size-style"
-                    sx={{
-                      color: "#2f7367",
-                      "& .MuiSlider-track": {
-                        height: 16,
-                        borderRadius: 4,
-                      },
-                      "& .MuiSlider-rail": {
-                        height: 16,
-                        borderRadius: 4,
-                        backgroundColor: "#bbd5d1",
-                      },
-                      "& .MuiSlider-thumb": {
-                        zIndex: 99,
-                      },
-                    }}
-                  />
-                </div>
-                <Box className="slider-size-displayer">
-                  {" "}
-                  <span>{height}</span>
-                  <span>px</span>
-                </Box>
-              </div>
+                Height
+              </Typography>
+              <Slider
+                value={height}
+                onChange={handleHeightChange}
+                min={256}
+                max={2048}
+                step={256}
+                className="slider-size-style"
+                sx={{
+                  color: "#2f7367",
+                  "& .MuiSlider-track": {
+                    height: 16,
+                    borderRadius: 4,
+                  },
+                  "& .MuiSlider-rail": {
+                    height: 16,
+                    borderRadius: 4,
+                    backgroundColor: "#bbd5d1",
+                  },
+                  "& .MuiSlider-thumb": {
+                    zIndex: 99,
+                  },
+                }}
+              />
+              <Box className="slider-size-displayer">
+                {" "}
+                <span>{height}</span>
+                <span>px</span>
+              </Box>
             </Box>
           </Box>
         </div>
-        <div className="number-of-image-container">
-          <div className="tit-num-style">
-            <span className="title-span-style">Number of Images</span>
-            <Box className="slider-number-of-images">
-              <span>{number}</span>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            gap: "15px",
+            alignItems: "center",
+          }}
+        >
+          <Box
+            sx={{
+              width: "100%",
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              padding: "10px",
+            }}
+          >
+            <Typography variant="body1" sx={{ display: "flex", width: "auto" }}>
+              Number of Images
+            </Typography>
+            <Box className="slider-size-displayer">
+              <span>{count}</span>
             </Box>
-          </div>
-          <div className="num-image-slider-custom">
+          </Box>
+          <Box>
             <Slider
-              value={number}
-              onChange={handleNumberChange}
-              min={0}
-              max={2000}
+              value={count}
+              onChange={handleCountChange}
+              min={1}
+              max={20}
+              step={1}
               className="slider-size-style2"
               sx={{
                 color: "#2f7367",
@@ -307,52 +428,60 @@ export default function TextToImage() {
                 },
               }}
             />
-          </div>
-        </div>
+          </Box>
+        </Box>
         <div className="btn-create">
-          <button>Create</button>
-          <button>2 credits will be charged</button>
+          <button onClick={handleGenerateImage}>
+            {isLoading ? "Generating..." : "Create"}
+          </button>
         </div>
       </div>
       <div className="image-board">
         {images.map((src, index) => (
           <div className="card" key={index}>
-            <div className="icons-down-star-trash">
-              <div className="icons-down-star-trash-size">
-                <Image
-                  width={24}
-                  height={24}
-                  src="./assets/home/star.svg"
-                  alt="like-icon"
-                  draggable={false}
-                />
-              </div>
-              <div className="icons-down-star-trash-size">
-                <Image
-                  width={24}
-                  height={24}
-                  src="./assets/home/down.svg"
-                  alt="download-icon"
-                  draggable={false}
-                />
-              </div>
-              <div className="icons-down-star-trash-size">
-                <Image
-                  width={24}
-                  height={24}
-                  src="./assets/home/trash.svg"
-                  alt="delete-icon"
-                  draggable={false}
-                />
-              </div>
-            </div>
             <Image
               width={256}
               height={256}
-              src={src}
-              alt={`Image ${index + 1}`}
+              src={`data:image/png;base64,${src}`}
+              alt={`Generated Image ${index + 1}`}
               draggable={false}
             />
+
+            <div className="icon-container">
+              <div className="icons-down-star-trash-size">
+                <Image
+                  src="/assets/home/star.svg"
+                  alt="star icon"
+                  width={20}
+                  height={20}
+                  draggable={false}
+                  className="icon"
+                />
+              </div>
+              <div
+                onClick={() => handleDownload(src, index)}
+                className="icons-down-star-trash-size"
+              >
+                <Image
+                  src="/assets/home/down.svg"
+                  alt="download icon"
+                  width={20}
+                  height={20}
+                  draggable={false}
+                  className="icon"
+                />
+              </div>
+              <div className="icons-down-star-trash-size">
+                <Image
+                  src="/assets/home/trash.svg"
+                  alt="trash icon"
+                  width={20}
+                  height={24}
+                  draggable={false}
+                  className="icon"
+                />
+              </div>
+            </div>
           </div>
         ))}
       </div>
