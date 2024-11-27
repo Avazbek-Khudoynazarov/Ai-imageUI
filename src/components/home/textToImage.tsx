7de1c509f983632babcdc0c1724401edcb5116d1
Avazbek-Khudoynazarov <justdanny9812@gmail.com>
2024-11-27 14:57:32 +0900

    feat: https

--- src/components/home/textToImage.tsx
+++ src/components/home/textToImage.tsx
@@ -73,7 +73,7 @@ export default function TextToImage() {
     try {
       setIsLoading(true);
       const response = await axios.post(
-        "http://ai.yeongnam.com:7860/sdapi/v1/txt2img",
+        "https://ai.yeongnam.com:7860/sdapi/v1/txt2img",
         payload
       );
       setImages(response.data.images);
